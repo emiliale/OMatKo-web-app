673840520c70be371173365de89fb1df8e74f61c
github-anki <51297948+github-anki@users.noreply.github.com>
2020-03-23 16:06:55 +0100

    Update routes.js
    add change password
    
    Co-Authored-By: emiliale <emiliale@users.noreply.github.com>

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,12 +4,14 @@ import { Route } from "react-router-dom";
 import ArticleList from "./containers/ArticleListView";
 import ArticleDetail from "./containers/ArticleDetailView";
 import Login from "./containers/Login";
+import ChangePassword from "./containers/ChangePassword";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={ArticleList} />{" "}
     <Route exact path="/articles/:articleID/" component={ArticleDetail} />{" "}
     <Route exact path="/login/" component={Login} />{" "}
+    <Route exact path="/password/" component={ChangePassword} />{" "}
   </div>
 );
 
