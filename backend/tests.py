5dc57e386e3f71845fc7ff97a8382e7ee3051265
emiliale <54060962+emiliale@users.noreply.github.com>
2020-04-04 18:33:22 +0200

    Adding login and changePassword tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework import status
+from django.test import Client
+
 
 class LogInTest(TestCase):
     def setUp(self):
@@ -9,6 +12,23 @@ class LogInTest(TestCase):
         User.objects.create_user(**self.credentials)
     def test_login(self):
         # send login data
-        response = self.client.post('/login/', self.credentials, follow=True)
-        # should be logged in now
-        self.assertTrue(response.context['user'].is_active)
+        response = self.client.post('/rest-auth/login/', self.credentials, follow=True)
+
+        self.assertTrue(response.status_code, status.HTTP_200_OK)
+
+class ChangePasswordTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+
+    def test_change_password(self):
+        self.client.post('/rest-auth/login/', self.credentials, follow=True)
+        response = self.client.post('/rest-auth/password/change/', {
+            'new_password1': 'newpassword',
+            'new_password2': 'newpassword',
+            'old_password': 'secret'
+        }, follow=True)
+        self.user = User.objects.get(username="testuser")  # get user again so that you can see updated password
+        self.assertEquals(self.user.check_password("newpassword"), True)
