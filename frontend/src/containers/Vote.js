b222220336b291ec3581148fbc64378ddd50dfb7
Anna Szymanek <szymanek_a@wp.pl>
2020-04-18 20:59:09 +0200

    Create Vote.js

---
+++ frontend/src/containers/Vote.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Form, Input, Icon, Button } from 'antd';
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
+
+const FormItem = Form.Item;
