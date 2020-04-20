6d805f3c9afb34ac75e8b042bb5a0e7de1105968
Anna Szymanek <szymanek_a@wp.pl>
2020-04-20 14:53:27 +0200

    Update Vote.js

--- frontend/src/containers/Vote.js
+++ frontend/src/containers/Vote.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import { Form, Input, Icon, Button } from 'antd';
-import { connect } from 'react-redux';
-import * as actions from '../store/actions/auth';
+import React, { Component } from 'react';
+import { HeartOutlined } from '@ant-design/icons';
+import { Form, Input, Icon, Button, Rate } from 'antd';
 
 const FormItem = Form.Item;
+
+class Vote extends React.Component {
+    render(){
+        
+        return(
+            <Form>
+                <FormItem name="code" label="Kod prelekcji" rules={[{ required: true }]}>
+                <Input />
+                </FormItem>
+
+                <FormItem>
+                    <Rate defaultValue={3}  character={<HeartOutlined />} allowHalf/>
+                    <span className="rate-content">Ocena za merytorykę</span>
+                <FormItem/>
+                <FormItem>
+                    <Rate defaultValue={3} character={<HeartOutlined />} allowHalf/>
+                    <span className="rate-content">Ocena za prezentację</span>
+                </FormItem>
+                    <Button type="primary" htmlType="vote" style={{marginRight: '10px'}}>
+                        Zapisz
+                    </Button>
+                </FormItem>
+    
+            </Form>);
+  }
+}
+
+export default Vote;
+
