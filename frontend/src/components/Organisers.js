422b1a95cd04535bdec7bb3423e0a6aaeab520e5
Anna Szymanek <szymanek_a@wp.pl>
2020-05-25 09:59:50 +0200

    add organisers

---
+++ frontend/src/components/Organisers.js
@@ -0,0 +1,42 @@
+import React from "react";
+import axios from "axios";
+
+import { Card } from "antd";
+
+const env = process.env.NODE_ENV || "development";
+const serverUrl =
+    env === "development"
+        ? "http://127.0.0.1:8000"
+        : "https://omatko-app-backend.herokuapp.com";
+
+
+class Organizers extends React.Component {
+  state = {
+    organizers: []
+  };
+
+  fetchSponsors = () => {
+    axios.get(`${serverUrl}/apiOrganizer/`).then(res => {
+      this.setState({
+        organizers: res.data
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchOrganizers();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Card title={this.state.organizers.surname}>
+          <p> {this.state.organizers.first_name} </p>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default Organizers;
